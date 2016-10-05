a52d308c11a5a8d4d22eea05d7a4bd109bb13ab6
Ed Harcourt <ehar@ehar-gpu-project.stlawu.local>
2016-10-05 16:57:40 -0400

    update with connection manager logic

--- app/src/main/java/com/example/edharcourt/newchatclient/ConnectionManager.java
+++ app/src/main/java/com/example/edharcourt/newchatclient/ConnectionManager.java
@@ -2,67 +2,156 @@ package com.example.edharcourt.newchatclient;
 
 
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 /**
  * Created by edharcourt on 10/4/16.
  */
 
-public class ConnectionManager implements Runnable {
+public class ConnectionManager {
 
     MainActivity act = null;
     Handler uiThread = null;
     String ipaddr = null;
 
     Socket sock = null;
-    BufferedReader fromServer = null;
-    PrintWriter toServer = null;
+    ServerSocket server_sock = null;
 
-    public ConnectionManager(MainActivity act, Handler uiThread, String ipaddr) {
+    // if this is acting as a client
+    BufferedReader from = null;
+    PrintWriter to = null;
+    boolean acting_as_server = false;
+    private final String LOG_TAG = ConnectionManager.class.getName();
+    boolean connected = false;
+
+    public ConnectionManager(MainActivity act, Handler uiThread) {
+        this.connected = false;
         this.act = act;
         this.uiThread = uiThread;
-        this.ipaddr = ipaddr;
     }
 
-    @Override
-    public void run() {
+    Runnable server_thread = new Runnable() {
 
-        uiThread.post(new Runnable() {
-            @Override
-            public void run() {
-                act.receiving.append(act.getString(R.string.waiting));
-            }
-        });
+        @Override
+        public void run() {
 
-        try {
-            sock = new Socket(ipaddr, 12346);
-            fromServer =
-                new BufferedReader(
-                    new InputStreamReader(sock.getInputStream()));
-
-            toServer = new PrintWriter(sock.getOutputStream(), true);
-
-        } catch (IOException e) {
-            e.printStackTrace();
             try {
-                sock.close();
-                fromServer.close();
-                toServer.close();
-            } catch (IOException e1) {
-                e1.printStackTrace();
+                server_sock = new ServerSocket(12346);
+                sock = server_sock.accept();
+                from = new BufferedReader(
+                        new InputStreamReader(sock.getInputStream()));
+                to = new PrintWriter(sock.getOutputStream(), true);
             }
+            catch (SocketException e) {
+                Log.w(LOG_TAG, e.getMessage());
+
+                // fine, might have been closed because we
+                // are running as the client and not the server
+                if (connected)
+                    return;
+                else
+                    Toast.makeText(act, "Something bad happened", Toast.LENGTH_LONG).show();
+            }
+            catch (IOException e) {
+                Log.w(LOG_TAG, e.getMessage());
+                uiThread.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        act.receiving.append("Error creating server socket.");
+                    }
+                });
+                e.printStackTrace();
+                if (server_sock.isBound()) {
+                    try {
+                        server_sock.close();
+                        sock.close();
+                        from.close();
+                        to.close();
+                    } catch (IOException e1) {
+                        Log.w(LOG_TAG, e1.getMessage());
+                        uiThread.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                act.receiving.append("Connection closed");
+                            }
+                        });
+                        e1.printStackTrace();
+                    }
+                }
+                return;
+            }
+
+            uiThread.post(new Runnable() {
+                @Override
+                public void run() {
+                    act.receiving.append(act.getString(R.string.connection_established));
+                }
+            });
+
+            acting_as_server = true;
+            connected = true;
+            notifyAll();
         }
-        finally {
+    };
 
+    Runnable client_thread = new Runnable() {
+        @Override
+        public void run() {
+            uiThread.post(new Runnable() {
+                @Override
+                public void run() {
+                    act.receiving.append(act.getString(R.string.waiting));
+                }
+            });
+
+            try {
+                sock = new Socket(ipaddr, 12346);
+                from = new BufferedReader(
+                           new InputStreamReader(sock.getInputStream()));
+
+                to = new PrintWriter(sock.getOutputStream(), true);
+
+            } catch (IOException e) {
+
+                Log.w(LOG_TAG, e.getMessage());
+                uiThread.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        act.receiving.append(act.getString(R.string.error_connecting));
+                    }
+                });
+
+                e.printStackTrace();
+                try {
+                    sock.close();
+                    from.close();
+                    to.close();
+                } catch (IOException e1) {
+                    Log.w(LOG_TAG, e1.getMessage());
+                }
+                return;
+            }
+
+            uiThread.post(new Runnable() {
+                @Override
+                public void run() {
+                    act.receiving.append(act.getString(R.string.connection_established));
+                }
+            });
+
+            acting_as_server = false;
+            connected = true;
+            notifyAll();
         }
-    }
-
-    Socket getSocket() { return sock; }
-
+    };
 
 }
--- app/src/main/java/com/example/edharcourt/newchatclient/MainActivity.java
+++ app/src/main/java/com/example/edharcourt/newchatclient/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.edharcourt.newchatclient;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     Button connect = null;
     TextView ip_address = null;
     TextView receiving = null;
+
+    ConnectionManager cm = null;
+    Handler uiThreadHandler = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +25,34 @@ public class MainActivity extends Activity {
         connect = (Button) findViewById(R.id.connect);
         ip_address = (TextView) findViewById(R.id.ip_address);
         receiving = (TextView) findViewById(R.id.receiving_text);
+        uiThreadHandler = new Handler();
 
         ip_address.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
+                // Fire off a thread that tries to connect to a server
+
+                // Don't create a connection if we are already connected
+                if (cm != null && cm.server_sock.isBound()) {
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Already connected",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (cm == null) {
+                    cm = new ConnectionManager(MainActivity.this, uiThreadHandler,
+                            ip_address.getText().toString());
+                    new Thread(cm.client_thread).start();
+                }
             }
         });
+
+
+
 
     }
 
